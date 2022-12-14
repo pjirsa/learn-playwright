1b78b6c73209f5bf2af76f582c203f7533f78bef
Phil Jirsa <phil.jirsa@microsoft.com>
2022-12-14 10:23:40 -0600

    use playwright-dotnet tests

---
+++ PlaywrightTests/NavigationTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class NavigationTest : PageTest
+{
+    const string URL = "http://localhost:5085/";
+
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync(URL);
+    }
+
+    [Test]
+    public async Task HomepageShouldHaveExpectedUrl()
+    {
+        await Expect(Page).ToHaveURLAsync(URL);
+    }
+}
